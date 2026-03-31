3987f1c265d235b43a6fb65855507532b4034eac
Vishnu <vishnu@seasiaconnect.com>
2026-03-31 15:31:39 +0530

    frame

--- tests/frame.spec.js
+++ tests/frame.spec.js
@@ -1,8 +1,23 @@
 const {test,expect} = require('@playwright/test');
 
 test('Frame test', async ({page}) => {  
-         await page.goto('https://demoqa.com/frames');
-         await page.frameLocator('#frame1');
-         await expect(frame.locator('#sampleHeading')).toHaveText('This is a sample page');
+         //await page.goto('https://demoqa.com/frames');
+            await page.goto('https://demoqa.com/nestedframes');
+
+         
+        // const childFrame=page.frameLocator('#frame2');
+         const parentFrame = page.frameLocator('#frame1');
+
+          await expect(parentFrame.locator('body'))
+           .toContainText('Parent frame');
+
+           const childFrame=parentFrame.frameLocator('iframe');
+              await expect(childFrame.locator('p')).toHaveText('Child Iframe');
+         
+        
+
+        //  await expect(frame1.locator('#sampleHeading')).toHaveText('This is a sample page');
+        //  await expect(frame2.locator('#sampleHeading')).toHaveText('This is a sample page');
+         
 
 })
